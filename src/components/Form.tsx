2278ee02a2dc26147c329a345b10e8f5a3801338
Abdelghafour <ghafourragg@gmail.com>
2022-04-16 06:00:11 +0100

    made aria dby/val & p

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,15 +6,19 @@ const EMAIL_REGEX = /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 const Form: React.FC = () => {
   const userRef = useRef<HTMLInputElement>(null);
+  const errorRef = useRef<HTMLInputElement>(null);
 
   const [firstName, setFirstName] = useState("");
   const [validFirstName, setValidFirstName] = useState(false);
+  const [firstNameFocus, setFirstNameFocus] = useState(false);
 
   const [lastName, setLastName] = useState("");
   const [validLastName, setValidLastName] = useState(false);
+  const [lastNameFocus, setLastNameFocus] = useState(false);
 
   const [email, setEmail] = useState("");
   const [validEmail, setValidEmail] = useState(false);
+  const [emailFocus, setEmailFocus] = useState(false);
 
   const [password, setPassWord] = useState("");
   const [validPassword, setValidPassword] = useState(false);
@@ -47,39 +51,67 @@ const Form: React.FC = () => {
 
   return (
     <section className="form-container">
-      <form action="" onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <input
           className={validFirstName ? "valid" : "invalid"}
           ref={userRef}
           type="text"
           value={firstName}
           placeholder="First Name"
+          autoComplete="off"
           onChange={(e) => setFirstName(e.target.value)}
+          onFocus={() => setFirstNameFocus(!firstNameFocus)}
+          onBlur={() => setFirstNameFocus(!firstNameFocus)}
           required
+          aria-describedby="first-name"
+          aria-invalid={validFirstName ? "false" : "true"}
         />
+        <p
+          id="first-name"
+          className={!validFirstName && firstNameFocus ? "there" : "not-there"}
+        >
+          {!validFirstName ? "Invalid First name" : "all good"}
+        </p>
         <input
           className={validLastName ? "valid" : "invalid"}
           type="text"
           value={lastName}
-          placeholder="Last Name "
+          placeholder="Last Name"
+          autoComplete="off"
           onChange={(e) => setLastName(e.target.value)}
+          onFocus={() => setLastNameFocus(!lastNameFocus)}
+          onBlur={() => setLastNameFocus(!lastNameFocus)}
           required
+          aria-describedby="last-name"
+          aria-invalid={validLastName ? "false" : "true"}
         />
+        <p
+          id="last-name"
+          className={!validLastName && lastNameFocus ? "there" : "not-there"}
+        >
+          {!validLastName ? "Invalid Last name" : "all good"}
+        </p>
         <input
           className={validEmail ? "valid" : "invalid"}
           type="email"
           value={email}
           placeholder="Email Address"
+          autoComplete="off"
           onChange={(e) => setEmail(e.target.value)}
+          onFocus={() => setEmailFocus(!emailFocus)}
+          onBlur={() => setEmailFocus(!emailFocus)}
           required
+          aria-invalid={validEmail ? "false" : "true"}
         />
         <input
           className={validPassword ? "valid" : "invalid"}
           type="password"
           value={password}
           placeholder="Password"
+          autoComplete="off"
           onChange={(e) => setPassWord(e.target.value)}
           required
+          aria-invalid={validPassword ? "false" : "true"}
         />
         <input type="submit" value="claim your free trial" />
       </form>
